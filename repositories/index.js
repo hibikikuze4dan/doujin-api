a9e5eb6963b605254c89d991bb338bac23942b3b
Austin Reed <acreed1998@gmail.com>
2026-05-17 17:19:37 -0500

    feat: create utils for users table

--- repositories/index.js
+++ repositories/index.js
@@ -2,10 +2,12 @@ const { initCollectionQueries } = require("./collections-repositories");
 const { initArchivesQueries } = require("./archives-repositories");
 const { initHistoryQueries } = require("./history-repositories");
 const { initTagsQueries } = require("./tags-repositories");
+const { initUserQueries } = require("./users-repositories");
 
 module.exports = {
   initCollectionQueries,
   initArchivesQueries,
   initHistoryQueries,
   initTagsQueries,
+  initUserQueries,
 };
