73cc3b8562ddaf6f7e68f4f49e578a2c23b83aa5
Austin Reed <acreed1998@gmail.com>
2026-05-25 16:24:45 -0500

    chore: continue ts migration

--- types/database.ts
+++ types/database.ts
@@ -16,3 +16,46 @@ export interface ArchiveWithConnectedTableData extends Archive {
 export type ArchiveTableGetResponse = ArchiveWithConnectedTableData | undefined;
 
 export type ArchiveTableAllRespnse = ArchiveWithConnectedTableData[];
+
+export interface Tag {
+  id: number;
+  archive_id: number;
+  name: string;
+  namespace: string;
+}
+
+export interface ArchiveHistory {
+  id: number;
+  archive_id: number;
+  last_page: number;
+  accessed_at: number;
+}
+
+export interface User {
+  id: number;
+  created_at: string;
+  username: string;
+  password: string;
+  salt: string;
+}
+
+export interface ArchiveRating {
+  id: number;
+  archive_id: number;
+  user_id: number;
+  rating: number;
+  rated_at: string;
+}
+
+export interface Collection {
+  id: number;
+  name: string;
+  description: string;
+  date_added: string;
+}
+
+export interface CollectionArchive {
+  colleciton_id: number;
+  archive_id: number;
+  date_added: string;
+}
