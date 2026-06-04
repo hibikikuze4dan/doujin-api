05b7117efeb0e96b5289eee7196aae03b46ff44d
Austin Reed <acreed1998@gmail.com>
2026-06-03 20:43:23 -0500

    chore: migrate app and server file

---
+++ src/server.ts
@@ -0,0 +1,64 @@
+#!/usr/bin/env node
+
+import http from "http";
+import debug from "debug";
+
+import app from "./app";
+const log = debug("doujin-api:server");
+
+function normalizePort(val: string | number) {
+  const port = typeof val === "string" ? parseInt(val, 10) : val;
+
+  if (Number.isNaN(port)) {
+    return val;
+  }
+
+  if (port >= 0) {
+    return port;
+  }
+
+  return false;
+}
+
+function onError(error: NodeJS.ErrnoException) {
+  if (error.syscall !== "listen") {
+    throw error;
+  }
+
+  const bind = typeof port === "string" ? `Pipe ${port}` : `Port ${port}`;
+
+  switch (error.code) {
+    case "EACCES":
+      console.error(`${bind} requires elevated privileges`);
+      process.exit(1);
+      break;
+    case "EADDRINUSE":
+      console.error(`${bind} is already in use`);
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+function onListening() {
+  const addr = server.address();
+  const bind = typeof addr === "string" ? `pipe ${addr}` : `port ${addr?.port}`;
+  log(`Listening on ${bind}`);
+}
+
+const port = normalizePort(process.env.PORT || "3000");
+
+if (port === false) {
+  throw new Error("Invalid port configuration");
+}
+
+const listenPort = typeof port === "string" ? parseInt(port, 10) : port;
+
+app.set("port", port);
+
+const server = http.createServer(app);
+
+server.listen(listenPort, "0.0.0.0");
+server.on("error", onError);
+server.on("listening", onListening);
