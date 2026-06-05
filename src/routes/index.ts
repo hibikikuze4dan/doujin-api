9ddde5aeb03c3dcb22cf92d402da44e49a17c521
Austin Reed <acreed1998@gmail.com>
2026-06-04 19:43:18 -0500

    feat: add nodemon for easier server development

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -7,4 +7,8 @@ router.get("/", function (req, res, _next) {
   res.render("index", { title: "Express" });
 });
 
+router.get("/health", function (req, res, _next) {
+  res.json("Working as intended");
+});
+
 export default router;
